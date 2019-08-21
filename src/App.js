28fa4bc28b30109f1a264b7ff9fbcaf6016a2712
Deven Swiergiel <Deven.Swiergiel@its.ny.gov>
2019-08-21 14:42:53 -0400

    Fixes css bug and add clear button

--- src/App.js
+++ src/App.js
@@ -12,11 +12,18 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
   }
 
   // Search Github users
   searchUsers = async text => {
+    this.setState({ loading: true });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&?client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
@@ -25,13 +32,23 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  showClear = () => {};
+
   render() {
+    const { users, loading } = this.state;
     return (
       <Fragment>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </Fragment>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onChange = e => {
@@ -16,18 +23,30 @@ export class Search extends Component {
   };
 
   render() {
+    // javascript destructuring props
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="form">
+        <form onSubmit={this.onSubmit} className="form grid-2">
           <input
             type="text"
             name="text"
             placeholder="Search Users..."
+            className="grid-4"
             value={this.state.text}
             onChange={this.onChange}
           />
-          <input type="submit" value="Search" className="btn-dark btn-block" />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
