b56bd5e4a877f1acf0fde122ecbff02e058917d8
Deven Swiergiel <Deven.Swiergiel@its.ny.gov>
2019-08-21 10:56:45 -0400

    Adds basic scaffolding for user grid and useritems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +8,9 @@ class App extends Component {
     return (
       <Fragment>
         <Navbar />
-        <h1>Hello</h1>
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </Fragment>
     );
   }
